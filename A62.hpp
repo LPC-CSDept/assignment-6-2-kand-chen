2ca33c45f00e80c4a185fa7da547eb03d1a031bf
kand-chen <ilovegreen109@gmail.com>
2022-10-04 17:55:16 -0700

    test1

--- A62.hpp
+++ A62.hpp
@@ -1,17 +1,37 @@
-//
-
 #include <iomanip>
 #include <iostream>
 using namespace std;
 
+
 // complete two functions
-double nthpowerp(int n)
+double nthpowerp(int M)
 {
         // return n_th power number
+    float result;
+    int i;
+    
+    result = 1;
+    for (i = 0;i < M; i++){
+         result *= 2; 
+    }
+
+    return result;
+
 }
-double nthpowern(int n)
+double nthpowern(int m)
 {
-        // return n_th power number
+         // return n_th power number
+    float result, i;
+
+    m = -1*m;
+
+    result = 1;
+    for (i = 0;i < m; i++){
+         result *= 2; 
+    }
+        result = 1 / result;
+    
+    return result;
 }
 void printout(double result)
 {
